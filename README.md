1cdca4ff16c8b7185ecc76135ca4b6eb40651476
binss <babywolf1@163.com>
2015-06-16 22:00:28 +0800

    写sublime_build.py方便带参数编译

---
+++ README.md
@@ -0,0 +1,12 @@
+# sublime_plugin
+
+### sublime_build.py
+
+Build and run the file with args. Support c and python.
+
+**Usage**: **Browse the Packages** and then copy **sublime_build.py** to the **User** folder
+
+You can set the keymap, the command of it is **build_with_args**, for example: 
+
+`{ "keys": ["super+shift+b"], "command": "build_with_args" }`
+
---
+++ sublime_build.py
@@ -0,0 +1,30 @@
+import sublime
+import sublime_plugin
+import os
+
+
+class BuildWithArgsCommand(sublime_plugin.WindowCommand):
+
+    def on_cancel(self):
+        sublime.status_message("Compile aborted")
+
+    def on_get_args(self, args=None):
+        self.command += args
+        # sublime.message_dialog(self.command)
+        self.window.run_command("exec", {"cmd": ["bash", "-c", self.command]})
+
+    def run(self, *args, **kwargs):
+        file_path = self.window.active_view().file_name()
+        split = os.path.splitext(file_path)
+        target_file_path = split[0]
+        file_type = split[1]
+        if file_type == ".c":
+            # 如果为unp的例子，加上链接库-lunp
+            if self.window.active_view().find("unp.h", 0, sublime.IGNORECASE):
+                self.command = "gcc '" + file_path + "'  -lunp -o  '" + target_file_path + "' && '" + target_file_path + "' "
+            else:
+                self.command = "gcc '" + file_path + "' -o  '" + target_file_path + "' && '" + target_file_path + "' "
+        elif file_type == ".py":
+            self.command = "python " + file_path + " "
+        self.window.show_input_panel("Input args(split by SPACE):", "", self.on_get_args, None, self.on_cancel)
+
